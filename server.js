8a2c0c296a69436825fe16c3d7321f41bd0a0b62
Dexter Lewis <dexterlewis@gmail.com>
2017-04-17 18:10:45 -0400

    add ejs templating engine

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+
+
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
 });
