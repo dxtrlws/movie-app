2accf532e8c0e047577ce313a6fc98d5d53f0af7
Dexter Lewis <dexterlewis@gmail.com>
2017-04-17 21:50:02 -0400

    remove index html and use ejs templating

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
