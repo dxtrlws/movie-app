7b38cd167369710d1a458921e361fa0139d608a7
Dexter Lewis <dexterlewis@gmail.com>
2017-05-01 20:06:57 -0400

    add new tests

--- controllers/movieController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-const jasonParser = bodyParser.json();
-const User = require('./../models/users');
-
-
-module.exports = router;
---
+++ test/test-index.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../server');
+chai.use(chaiHttp);
+
+
+describe('Movie app', function() {
+    it('Should return status of 200 on homepage', function() {
+        return chai.request(server)
+            .get('/')
+            .then(function(res) {
+                res.should.have.status(200);
+            });
+    });
+    it('Should return status of 200 on user login', function() {
+        return chai.request(server)
+            .get('/users/login')
+            .then(function(res) {
+                res.should.have.status(200);
+            });
+    });
+    it('Should return status of 200 on registration', function() {
+        return chai.request(server)
+            .get('/users/register')
+            .then(function(res) {
+                res.should.have.status(200);
+            });
+    });
+
+});
